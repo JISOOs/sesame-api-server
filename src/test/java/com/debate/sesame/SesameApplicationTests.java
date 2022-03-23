c8626b35e59e7bae786066ae78ed17b2bb0476d2
jisoo shin <jjns97@mju.ac.kr>
2022-03-23 18:54:21 +0900

    initial commit

---
+++ src/test/java/com/debate/sesame/SesameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debate.sesame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
